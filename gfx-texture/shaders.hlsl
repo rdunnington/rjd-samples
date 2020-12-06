193db530ea2f93b2230219f5f5d3ffba02d0d5d0
Reuben Dunnington <reuben.dunnington@gmail.com>
2020-12-05 22:42:40 -0800

    * gfx-texture and gfx-interleaved d3d11 port * take latest rjd for clearcolor fix

--- gfx-texture/shaders.hlsl
+++ gfx-texture/shaders.hlsl
@@ -1,37 +1,40 @@
 cbuffer Uniforms : register(b0)
 {
-    float4x4 projectionMatrix;
-    float4x4 modelViewMatrix;
+	float4x4 matrix_projection;
+	float4x4 matrix_modelview;
 };
+
+Texture2D tex : register( t0 );
+sampler linear_sampler = sampler_state{};
 
 struct VS_INOUT
 {
-    float4 position : SV_Position;
-    float4 color : COLOR;
+	float4 position : SV_Position;
+	float2 uv : TEXCOORD;
 };
 
 struct PS_OUTPUT
 {
-    float4 color : SV_Target;
+	float4 color : SV_Target;
 };
 
 VS_INOUT vertexShader(VS_INOUT input)
 {
-    VS_INOUT output;
+	VS_INOUT output;
 
-    // transpose the matrices to get them into row-major format (rjd_math is column major)
-    float4x4 transform = mul(transpose(modelViewMatrix), transpose(projectionMatrix));
+	// transpose the matrices to get them into row-major format (rjd_math is column major)
+	float4x4 transform = mul(transpose(matrix_modelview), transpose(matrix_projection));
 
-    float4 pos = float4(input.position.xyz, 1);
-    output.position = mul(pos, transform);
-	output.color = input.color;
+	float4 pos = float4(input.position.xyz, 1);
+	output.position = mul(pos, transform);
+	output.uv = input.uv;
 
-    return output;
+	return output;
 }
 
 PS_OUTPUT pixelShader(VS_INOUT input)
 {
-    PS_OUTPUT output;
-    output.color = input.color;
-    return output;
+	PS_OUTPUT output;
+	output.color = tex.Sample(linear_sampler, input.uv);
+	return output;
 }
